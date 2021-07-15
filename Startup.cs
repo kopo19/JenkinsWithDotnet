4b06a8124066d72d87782385e08920dd49805e7f
juhyeun <hbe0476@naver.com>
2021-07-15 17:14:03 +0900

    3rd

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace JenkinsWithDotnet
             {
                 endpoints.MapGet("/", async context =>
                 { 
-                    await context.Response.WriteAsync("Hello Juhyeun 7 ^^ !");
+                    await context.Response.WriteAsync("Hello Juhyeun 3 ^^ !");
                 });
             });
         }
